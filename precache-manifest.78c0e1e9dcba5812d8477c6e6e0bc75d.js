c8b89c94ea9c99bbc22a5c7cd3b465f156a982b3
leye195 <leye195@naver.com>
2020-03-10 16:45:33 +0900

    Updates

---
+++ precache-manifest.78c0e1e9dcba5812d8477c6e6e0bc75d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78a852a4a1b720693c353e714003c67",
+    "url": "/HappReads-front/index.html"
+  },
+  {
+    "revision": "6e50e344e9b4872d345f",
+    "url": "/HappReads-front/static/css/main.b5f21a20.chunk.css"
+  },
+  {
+    "revision": "c84db3b8e9812fea1e27",
+    "url": "/HappReads-front/static/js/2.0ccd20bd.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/HappReads-front/static/js/2.0ccd20bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e50e344e9b4872d345f",
+    "url": "/HappReads-front/static/js/main.99cece7d.chunk.js"
+  },
+  {
+    "revision": "4e3e1997e9b2df56690a",
+    "url": "/HappReads-front/static/js/runtime-main.a81acd66.js"
+  },
+  {
+    "revision": "f081dab03804b4a6954b79ac82bb48f7",
+    "url": "/HappReads-front/static/media/1.f081dab0.jpg"
+  },
+  {
+    "revision": "621371455d4f6e55e3ba68d0daa30e64",
+    "url": "/HappReads-front/static/media/go_back.62137145.png"
+  },
+  {
+    "revision": "443a21898793835aa3fe61d1df188844",
+    "url": "/HappReads-front/static/media/tom-hermans-9BoqXzEeQqM-unsplash.443a2189.jpg"
+  }
+]);
