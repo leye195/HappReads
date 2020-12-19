e188c43875d4f7096f768d445803efac3e595af0
leye195 <leye195@naver.com>
2020-12-19 18:26:36 +0900

    # js to jsx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./route/Home";
-import Login from "./route/Login/Login";
-import SignUp from "./route/SignUp/SignUp";
-import Profile from "./route/Profile";
-import Detail from "./route/Detail";
-import Footer from "./components/Footer";
-import Search from "./route/Search";
-import Upload from "./route/Upload/Upload";
 import Header from "./components/Header";
-import NoMatch from "./route/NoMatch";
-import BookShelve from "./components/BookShelve/BookShelve";
-import Community from "./route/Community";
+import Footer from "./components/Footer";
 import FloatingButton from "./components/FloatingButton/FloatingButton";
+import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Profile from "./pages/Profile";
+import Detail from "./pages/Detail";
+import Search from "./pages/Search";
+import Upload from "./pages/Upload/Upload";
+import NoMatch from "./pages/NoMatch";
+import BookShelve from "./components/BookShelve/BookShelve";
+import Community from "./pages/Community";
 import "./styles/globalStyle.scss";
 
 function App() {
